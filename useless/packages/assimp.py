92306cbecf9b62ca9619f66eceb52be4c14e7bcb
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2021-01-22 00:50:27 -0500

    add assimp

---
+++ useless/packages/assimp.py
@@ -0,0 +1,15 @@
+from useless import base
+from useless.base import *
+
+
+class Solver(CMakePackage):
+    def __init__(self):
+        super().__init__()
+        self.name = 'assimp'
+        self.set('ASSIMP_BUILD_TESTS', "OFF")
+        self.set('BUILD_SHARED_LIBS', "OFF")
+        self.depends(resolve_package('zlib'))
+
+    def download(self):
+        self.checkpoint('download', lambda: download_git(
+            'https://github.com/assimp/assimp', self.src_dir))
