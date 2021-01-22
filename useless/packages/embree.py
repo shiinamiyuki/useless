944a519bb6dcee9361b9f2a111925bdfa51d31e3
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2021-01-22 00:45:35 -0500

    update embree

---
+++ useless/packages/embree.py
@@ -0,0 +1,15 @@
+from useless import base
+from useless.base import *
+
+
+class Solver(CMakePackage):
+    def __init__(self):
+        super().__init__()
+        self.name = 'embree'
+        self.set('EMBREE_TASKING_SYSTEM', 'INTERNAL')
+        self.set('EMBREE_ISPC_SUPPORT', 'OFF')
+        self.set('EMBREE_TUTORIALS', 'OFF')
+
+    def download(self):
+        self.checkpoint('download', lambda: download_git(
+            'https://github.com/embree/embree', self.src_dir))
