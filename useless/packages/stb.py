7ea9f0fbfc2b141b9b1a04551ff962c218fc4935
shiinamiyuki <xiaochun.tong@mail.utoronto.ca>
2021-01-22 01:36:59 -0500

    fix stb

--- useless/packages/stb.py
+++ useless/packages/stb.py
@@ -8,9 +8,10 @@ project(stb LANGUAGES C)
 include(GNUInstallDirs)
 add_library(stb_image stb_image.h stb_image.c)
 add_library(stb::image ALIAS stb_image)
+add_library(stb_image_write stb_image_write.h stb_image_write.c)
+add_library(stb::image_write ALIAS stb_image)
 
-
-install(TARGETS stb_image 
+install(TARGETS stb_image stb_image_write
     EXPORT stb-config
     DESTINATION lib)
 
@@ -31,6 +32,10 @@ stb_image_c = """
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 """
+stb_image_write_c = """
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+"""
 
 
 class Solver(CMakePackage):
@@ -46,4 +51,6 @@ class Solver(CMakePackage):
                 f.write(cmake_src)
             with open(self.src_dir+'stb_image.c', 'w') as f:
                 f.write(stb_image_c)
+            with open(self.src_dir+'stb_image_write.c', 'w') as f:
+                f.write(stb_image_write_c)
         self.checkpoint('download', F)
